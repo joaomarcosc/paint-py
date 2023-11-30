fb107eae413780da00f00d06f7d58e1495199a1f
joaomarcos <j.marcos9@hotmail.com>
2023-11-30 16:32:25 -0300

    feat: change lib to get a better GUI

--- paint.py
+++ paint.py
@@ -1,63 +1,62 @@
-import pygame
-import sys
+import tkinter as tk
+from tkinter import colorchooser
 
-pygame.init()
+class PaintApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Projeto computacao grafica")
+        
+        self.pen_color = "black"
+        self.eraser_color = "white"
+        self.pen_size = 2
+        self.eraser_mode = False
 
-WIDTH, HEIGHT = 800, 600
-BACKGROUND_COLOR = (255, 255, 255)
-MIN_DRAW_SIZE, MAX_DRAW_SIZE = 1, 20
-DRAW_SIZE = 5
+        self.canvas = tk.Canvas(root, bg="white", width=800, height=600)
+        self.canvas.pack(expand=tk.YES, fill=tk.BOTH)
+        
+        self.create_navbar()
+        
+        self.canvas.bind("<B1-Motion>", self.paint)
+        self.canvas.bind("<ButtonRelease-1>", self.reset)
 
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-WHITE = (255, 255, 255)
+    def create_navbar(self):
+        navbar_frame = tk.Frame(self.root, bd=5, relief=tk.RAISED)
+        navbar_frame.pack(side=tk.TOP, fill=tk.X)
+        
+        color_btn = tk.Button(navbar_frame, text="Cores", command=self.choose_color)
+        color_btn.pack(side=tk.LEFT, padx=5)
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Aplicação Paint")
+        eraser_btn = tk.Button(navbar_frame, text="Borracha", command=self.toggle_eraser)
+        eraser_btn.pack(side=tk.LEFT, padx=5)
+        
+        size_scale = tk.Scale(navbar_frame, from_=1, to=10, orient=tk.HORIZONTAL, label="Tamanho", command=self.set_size)
+        size_scale.pack(side=tk.LEFT, padx=5)
 
-canvas = pygame.Surface((WIDTH, HEIGHT))
-canvas.fill(BACKGROUND_COLOR)
+    def choose_color(self):
+        color = colorchooser.askcolor()[1]
+        if color:
+            self.pen_color = color
+            self.eraser_mode = False
 
-draw_color = BLACK
-draw_size = DRAW_SIZE
+    def toggle_eraser(self):
+        self.eraser_mode = not self.eraser_mode
+        if self.eraser_mode:
+            self.pen_color = self.eraser_color
+        else:
+            self.pen_color = colorchooser.askcolor()[1] 
 
-font = pygame.font.Font(None, 36)
+    def set_size(self, val):
+        self.pen_size = int(val)
 
-drawing = False
-running = True
+    def paint(self, event):
+        x1, y1 = (event.x - self.pen_size), (event.y - self.pen_size)
+        x2, y2 = (event.x + self.pen_size), (event.y + self.pen_size)
+        self.canvas.create_oval(x1, y1, x2, y2, fill=self.pen_color, outline=self.pen_color)
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            drawing = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            drawing = False
-        elif event.type == pygame.MOUSEMOTION and drawing:
-            pos = pygame.mouse.get_pos()
-            pygame.draw.circle(canvas, draw_color, pos, draw_size)
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_r:
-                draw_color = RED
-            elif event.key == pygame.K_g:
-                draw_color = GREEN
-            elif event.key == pygame.K_b:
-                draw_color = BLUE
-            elif event.key == pygame.K_y:
-                draw_color = YELLOW
-            elif event.key == pygame.K_w:
-                draw_color = WHITE
-            elif event.key == pygame.K_UP:
-                draw_size = min(MAX_DRAW_SIZE, draw_size + 1)
-            elif event.key == pygame.K_DOWN:
-                draw_size = max(MIN_DRAW_SIZE, draw_size - 1)
+    def reset(self, event):
+        pass
 
-    screen.blit(canvas, (0, 0))
-    pygame.display.flip()
-
-pygame.quit()
-sys.exit()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PaintApp(root)
+    root.mainloop()
